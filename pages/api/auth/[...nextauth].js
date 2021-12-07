b95ec5e9a67ed485a0223feeb1d6fd07479b18ab
mecode <mecode670@gmail.com>
2021-12-07 19:45:33 +0530

    Prisma | paperAt | Added Prisma #1

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from `next-auth/providers/google`
+import EmailProvider from `next-auth/providers/email`
 providers: [
   GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID,
